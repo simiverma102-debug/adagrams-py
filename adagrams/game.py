e0a58cbe411b5d836c629d3e3cbcec315161f596
Simran Verma <simi.verma.102@gmail.com>
2026-09-09 15:01:42 -0700

    wave_01 passed

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,7 +1,49 @@
 from random import randint
+#import random
 
 def draw_letters():
-    pass
+    LETTER_POOL = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+    }
+    hand = []
+
+    for i in range(10):
+        index = randint(0, len(LETTER_POOL) - 1)
+        letter = list(LETTER_POOL.keys())[index]
+
+        hand.append(letter)
+        LETTER_POOL[letter] -= 1
+        if LETTER_POOL[letter] == 0:
+            del LETTER_POOL[letter]
+
+    return hand
+    
+
 
 def uses_available_letters(word, letter_bank):
     pass
